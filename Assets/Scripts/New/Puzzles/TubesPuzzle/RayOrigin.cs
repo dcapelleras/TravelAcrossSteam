2a8b2821fe1960cef1d70affffc2c276fb4a65be
dcapelleras <dcapelleras@gmail.com>
2023-03-31 14:29:38 +0200

    fixed conductor system

--- Assets/Scripts/New/Puzzles/TubesPuzzle/RayOrigin.cs
+++ Assets/Scripts/New/Puzzles/TubesPuzzle/RayOrigin.cs
@@ -5,19 +5,39 @@ using UnityEngine;
 
 public class RayOrigin : MonoBehaviour
 {
+
+    float timeConnected;
+    float maxTimeConnected = 1f;
+    public bool connected;
     //to make it work, only activate ray when a piece rotates, then disconnect all
     void Update()
     {
-        RaycastHit hit;//origin of the ray, always shoots
-        Debug.DrawRay(transform.position, transform.TransformDirection(UnityEngine.Vector3.up) * 1.5f, Color.yellow);
-        if (Physics.Raycast(transform.position, transform.TransformDirection(UnityEngine.Vector3.up), out hit, 1.5f)) //direction to start the ray
+        ActivateRay();
+        if (connected)
         {
-            if (hit.transform.TryGetComponent(out RayReceiver receiver))
+            timeConnected += Time.deltaTime;
+            if (timeConnected >= maxTimeConnected )
             {
-                receiver.ActivateRay(true, null);
+                connected = false;
+                timeConnected= 0;
             }
         }
     }
 
-
+    public void ActivateRay()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            connected= true;
+            RaycastHit hit;//origin of the ray, always shoots
+            Debug.DrawRay(transform.position, transform.TransformDirection(UnityEngine.Vector3.up) * 1.5f, Color.yellow);
+            if (Physics.Raycast(transform.position, transform.TransformDirection(UnityEngine.Vector3.up), out hit, 1.5f)) //direction to start the ray
+            {
+                if (hit.transform.TryGetComponent(out RayReceiver receiver))
+                {
+                    receiver.ActivateRay();
+                }
+            }
+        }
+    }
 }
--- Assets/Scripts/New/Puzzles/TubesPuzzle/RayReceiver.cs
+++ Assets/Scripts/New/Puzzles/TubesPuzzle/RayReceiver.cs
@@ -4,90 +4,44 @@ using UnityEngine;
 
 public class RayReceiver : MonoBehaviour
 { //for a ending of the puzzle, create a new script with the same receive ray but a different outcome
-    public bool connectedToSource;
-    public bool connectedToPrevious;
-    float timeToCheck = 1f;
-    float timer;
+    public bool connected;
 
-    public GameObject previousObject;
+    [SerializeField] RayOrigin origin;
 
     private void Update()
     {
-        if (connectedToSource || connectedToPrevious)
+        if (!origin.connected)
+        {
+            connected = false;
+            return;
+        }
+        if (connected)
         {
             RaycastHit hit1;
             RaycastHit hit2;
             Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.up) * 1.5f, Color.yellow);
             Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.down) * 1.5f, Color.yellow);
 
-            if (!connectedToSource) //si no viene de la source
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.up), out hit1, 1.5f)) //si da el ray
             {
-                if (previousObject != null)
+                if (hit1.transform.TryGetComponent(out RayReceiver receiver)) //encuentra el receiver
                 {
-                    if (CheckPrevious(previousObject)) //rastrea hasta que encuentre o no la source siguiendo los rayos
-                    {
-                        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.up), out hit1, 1.5f)) //si da el ray
-                        {
-                            if (hit1.transform.TryGetComponent(out RayReceiver receiver)) //encuentra el receiver
-                            {
-                                receiver.ActivateRay(false, gameObject);
-                            }
-                        }
-                        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit1, 1.5f)) //si da el ray
-                        {
-                            if (hit1.transform.TryGetComponent(out RayReceiver receiver)) //encuentra el receiver
-                            {
-                                receiver.ActivateRay(false, gameObject);
-                            }
-                        }
-                    }
+                    receiver.ActivateRay();
                 }
             }
-
-           
-        }
-        if (connectedToPrevious || connectedToSource)
-        {
-            timer += Time.deltaTime;
-            if (timer > timeToCheck)
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit2, 1.5f)) //si da el ray
             {
-                connectedToSource= false;
-                connectedToPrevious=false;
+                if (hit2.transform.TryGetComponent(out RayReceiver receiver)) //encuentra el receiver
+                {
+                    receiver.ActivateRay();
+                }
+
             }
         }
     }
 
-    bool CheckPrevious(GameObject previous)
+    public void ActivateRay()
     {
-        if (previous == null)
-
-        {
-            return false;
-        }
-        if (previous.TryGetComponent(out RayReceiver receiver))
-        {
-            if (!receiver.connectedToSource)
-            {
-                CheckPrevious(receiver.previousObject);
-            }
-            else
-            {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public void ActivateRay(bool fromOrigin, GameObject previous)
-    {
-        if (fromOrigin)
-        {
-            connectedToSource = true;
-        }
-        else
-        {
-            connectedToPrevious = true;
-            previousObject = previous;
-        }
+        connected = true;
     }
 }
