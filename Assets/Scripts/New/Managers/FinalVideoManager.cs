15fbcaf33d3a840cb2b5f81f09c9a0f1283199a5
Dani <68642430+dcapelleras@users.noreply.github.com>
2023-07-05 15:32:51 +0200

    Finished adding last video

--- Assets/Scripts/New/Managers/FinalVideoManager.cs
+++ Assets/Scripts/New/Managers/FinalVideoManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.Video;
 using Yarn.Unity;
@@ -8,8 +9,7 @@ public class FinalVideoManager : MonoBehaviour
 {
     DialogueRunner runner;
     [SerializeField] List<GameObject> videoList;
-    [SerializeField] GameObject backgroundImage;
-
+    [SerializeField] GameObject lastButton;
     int currentDiapositiveIndex = 0;
 
     bool videoOn = false;
@@ -22,25 +22,34 @@ public class FinalVideoManager : MonoBehaviour
 
     public void StartVideo()
     {
-        backgroundImage.SetActive(true);
         videoOn= true;
         videoList[0].SetActive(true);
+        LastButtonAppear();
     }
 
+    public async void LastButtonAppear()
+    {
+        await Task.Delay(60000);
+        lastButton.SetActive(true);
+    }
+
+    /*
     private void Update()
     {
+        
         if (videoOn)
         {
             if (Input.GetKeyDown(KeyCode.Escape))
             {
-                videoList[videoList.Count].SetActive(true);
+                videoList[videoList.Count -1].SetActive(true);
             }
             if (Input.anyKeyDown)
             {
                 GoToNextDiapositive();
             }
         }
-    }
+}
+    */
 
     public void GoToNextDiapositive()
     {
